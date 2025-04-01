007503b0462fdf0cc8a0572bdfc30218b19baade
kersor <vasilev.rustik.03@mail.ru>
2025-04-01 23:11:40 +0300

    PRODUCT_PAGE: Add swiper

--- frontend/src/react/pages/slugs/pageSlugProduct/PageSlugProduct.tsx
+++ frontend/src/react/pages/slugs/pageSlugProduct/PageSlugProduct.tsx
@@ -1,9 +1,12 @@
+"use client"
+
+import { Container } from "@/react/components/containers/container/Container";
 import { SectionSwiperSlugProduct } from "@/react/sections/slugs/slugProduct/sectionSwiperSlugProduct/SectionSwiperSlugProduct";
 
 export default function PageSlugProduct () {
     return (
-        <div>
+        <Container >
             <SectionSwiperSlugProduct />
-        </div>
+        </Container>
     )
 }
--- frontend/src/react/sections/slugs/slugProduct/sectionSwiperSlugProduct/SectionSwiperSlugProduct.tsx
+++ frontend/src/react/sections/slugs/slugProduct/sectionSwiperSlugProduct/SectionSwiperSlugProduct.tsx
@@ -1,5 +1,81 @@
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react"
+
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+
+import styles from './styles.module.css'
+
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules'
+import { useState } from "react";
+import clsx from "clsx";
+import { ChevronLast, ChevronLeft, ChevronRight } from "lucide-react";
+
 export const SectionSwiperSlugProduct = () => {
+    const [thumbsSwiper, setThumbsSwiper] = useState<any>(null);
+
     return (
-        <div></div>
+        <div className={styles.wrapper}> 
+            <Swiper
+                loop={true}
+                spaceBetween={20}
+               
+                thumbs={{ swiper: thumbsSwiper }}
+                modules={[FreeMode, Navigation, Thumbs]}
+                className={styles.mySwiper2}
+            >
+                {
+                    [...Array(10)].map((_, index: number) => (
+                        <SwiperSlide key={index}>
+                            <img src="/product/1.png" alt="" />
+                        </SwiperSlide>
+                    ))
+                }
+                <SwiperNavigation />
+            </Swiper>
+            <Swiper
+                onSwiper={setThumbsSwiper}
+                // loop={true}
+                spaceBetween={10}
+                slidesPerView={6}
+                freeMode={true}
+                watchSlidesProgress={true}
+                modules={[FreeMode, Navigation, Thumbs]}
+                className={styles.mySwiper}
+            >
+                {
+                    [...Array(10)].map((_, index: number) => (
+                        <SwiperSlide key={index}>
+                            <img src="/product/1.png" alt="" />
+                            <div className={styles.mySwiper_back}></div>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
+        </div>
     )
 }
+
+const SwiperNavigation = () => {
+    const swiper = useSwiper();
+
+    const slidePrev = () => {
+        if (swiper) swiper.slidePrev();
+    };
+
+    const slideNext = () => {
+        if (swiper) swiper.slideNext();
+    };
+
+    return (
+        <>
+            <div onClick={slidePrev} className={clsx(styles.mySwiper_btn, styles.mySwiper_btn__prev)}>
+                <ChevronLeft strokeWidth={2} color="#FFF" width={20}/>
+            </div>
+            <div onClick={slideNext} className={clsx(styles.mySwiper_btn, styles.mySwiper_btn__next)}>
+                <ChevronRight strokeWidth={2} color="#FFF" width={20}/>
+            </div>
+        </>
+    );
+};
