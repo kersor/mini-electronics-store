e82a00f2c4fcb5360cc22933c624182dd1b1fd70
svolochyo <127143304+kersor@users.noreply.github.com>
2025-04-10 13:50:11 +0300

    PRODUCT_PAGE: Add specification

--- frontend/src/react/pages/slugs/pageSlugProduct/PageSlugProduct.tsx
+++ frontend/src/react/pages/slugs/pageSlugProduct/PageSlugProduct.tsx
@@ -3,12 +3,17 @@
 import { Container } from "@/react/components/containers/container/Container";
 import { SectionProductAside } from "@/react/sections/slugs/slugProduct/sectionProductAside/SectionProductAside";
 import { SectionSwiperSlugProduct } from "@/react/sections/slugs/slugProduct/sectionSwiperSlugProduct/SectionSwiperSlugProduct";
+import styles from './styles.module.css'
+import { SectionProductSpecifications } from "@/react/sections/slugs/slugProduct/sectionProductSpecifications/SectionProductSpecifications";
 
 export default function PageSlugProduct () {
     return (
-        <Container className="flex gap-10">
-            <SectionSwiperSlugProduct />
-            <SectionProductAside />
+        <Container className="flex flex-col gap-10 pb-10">
+            <div className="flex items-start gap-10">
+                <SectionSwiperSlugProduct />
+                <SectionProductAside />
+            </div>
+            <SectionProductSpecifications />
         </Container>
     )
 }
