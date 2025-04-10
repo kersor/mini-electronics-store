4a9d7c117f19c8fc2c7ed44b781051b3c34dc1a1
svolochyo <127143304+kersor@users.noreply.github.com>
2025-04-10 16:03:51 +0300

    FAVORITES_PAGE: Add функционал для избранных

--- frontend/src/react/sections/favorites/sectionListFavorites/SectionListFavorites.tsx
+++ frontend/src/react/sections/favorites/sectionListFavorites/SectionListFavorites.tsx
@@ -2,11 +2,14 @@
 
 import { CardCatalogProduct } from '@/react/components/cards/catalog/cardCatalogProduct/CardCatalogProduct'
 import styles from './styles.module.css'
+import { useFavorites } from '@/store/favorites.zustand'
 
 export const SectionListFavorites = () => {
+    const favorites = useFavorites(state => state)
+
     return (
         <div className={styles.favorites_wrapper}>
-            {[...Array(10)].map((item: any, index: number) => <CardCatalogProduct key={index} index={index} />)}
+            {favorites.favorites.map((item: any, index: number) => <CardCatalogProduct key={item.id} data={item}  />)}
         </div>
     )
 }
